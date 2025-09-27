1881ff42b519a51748d33115ceadbba2ee1768e6
Sugun-ravipalli <145236879+Sugun-ravipalli@users.noreply.github.com>
2025-09-27 04:25:27 +0000

    Save workspace changes (excluding rsa key)

--- generic_code/code_library.py
+++ generic_code/code_library.py
@@ -1,11 +1,19 @@
 from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col,to_timestamp
+from datetime import datetime
+import pytz
 
-def snowconnection(connection_config):
+def snowconnect(connection_config):
     session = Session.builder.configs(connection_config).create()
-    session_details = session.create_dataframe([[session._session_id,session.sql("select current_user();").collect()[0][0],str(session.get_current_warehouse()).replace('"',''),str(session.get_current_role()).replace('"','')]], schema=["session_id","user_name","warehouse","role"])
+    central = pytz.timezone("America/chicago")
+    session_details = session.create_dataframe([(str(session.session_id),\
+        session.sql("select current_user").collect()[0][0],\
+        str(session.get_current_warehouse()).replace('"',''),\
+        str(session.get_current_role()).replace('"',''),\
+        datetime.now(central).replace(tzinfo=None))], schema = ["session","User","warehouse","role","created_at"])
     session_details.write.mode("append").save_as_table("session_audit")
     return session
+
 
 
 def copy_to_table(session,config_file,schema='NA'):
--- makeconnection.py
+++ makeconnection.py
@@ -1,22 +1,56 @@
 import sys
 from snowflake.snowpark import Session
+sys.path.append("/workspaces/Snowpark_pipeline")
+import generic_code.code_library as cl
 
+import importlib
+import generic_code.code_library as cl
+importlib.reload(cl)
 # If you are running this code in Github code spaces you don't need to execute below command,
 #sys.path.append('/Users/pradeep/Downloads/Udemy_course_videos/course_2_assignments/Snowpark_pipeline/')
 
 from generic_code import code_library
+from cryptography.hazmat.primitives import serialization
+
+
+
+# Load private key
+with open("rsa_key.p8", "rb") as key:
+    p_key = serialization.load_pem_private_key(
+        key.read(),
+        password=None,
+    )
+
+private_key_bytes = p_key.private_bytes(
+    encoding=serialization.Encoding.DER,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption()
+)
 
 # Make connection and create Snowpark session.
 # Please mention your snowflake account credentials below,
-connection_parameters = {"account":"ijvunnh-ny22848", \
-"user":"pradeep", \
-"password": "AbcdAbcdAbcd067$", \
-"role":"ACCOUNTADMIN", \
-"warehouse":"COMPUTE_WH", \
-"database":"DEMO_DB", \
-"schema":"PUBLIC" \
+connection_parameters = {
+    "account":"KZYJRAW-EE11046",
+    "user":"SUGUNSNOWPARK",
+    "private_key": private_key_bytes,
+    "role": "ACCOUNTADMIN",
+    "warehouse": "COMPUTE_WH",
+    "database": "DEMO_DB",
+    "schema": "PUBLIC"
 }
 
 
 # Create connection with snowflake and return the session
-session = code_library.snowconnection(connection_parameters)
+session = cl.snowconnect(connection_parameters)
+
+session.session_id
+session.sql("select current_user").collect()[0][0]
+session.get_current_warehouse()
+session.get_current_role()
+
+
+
+# session.sql("truncate table  session_audit").collect()
+
+
+
